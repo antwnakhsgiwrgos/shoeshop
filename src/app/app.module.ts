a19e8eb56652a3dc34863b25efb5bb9dc9881c1b
antwnakhs giwrgos <antwnakhsgiwrgos@gmail.com>
2019-12-09 21:09:03 +0200

    shoeshop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http';
+
+
+const routes = [
+  {
+    path:"",
+    component: HomeComponent
+  },
+  {
+    path:"products",
+    component: ProductsComponent
+  },
+  {
+    path:"about",
+    component: AboutComponent
+  },
+  {
+    path:"contact",
+    component: ContactComponent
+  }
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductsComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
